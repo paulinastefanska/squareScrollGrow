126ed3a35155eba311d7166cba11594f0278ddad
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-11-23 14:34:58 +0100

    Add js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 const square = document.createElement('div');
 document.body.appendChild(square);
+
+let grow = true; //flag
+let size = 100; //square size
+square.style.width = size + "px"; //"100px"
+square.style.height = size + "px";
+
+window.addEventListener("scroll", function () {
+
+  if (size >= this.window.innerWidth * 0.2) { 
+    grow = !grow; //false
+  } else if (size <= 20) {
+    grow = !grow; //true
+  }
+
+  if (grow) { //true
+    size += 2;
+    square.style.width = size + "px";
+    square.style.height = size + "px";
+  } else {
+    size -= 2;
+    square.style.width = size + "px";
+    square.style.height = size + "px";
+  }
+})
